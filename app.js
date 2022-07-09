1bf7f452de087a891722af93876636c58ac2a12c
IhorZviahin <igorzvjagin26@gmail.com>
2022-07-09 23:59:48 +0300

    Lesson 7 Email by victor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+//require('dotenv').config()
+//require('dotenv').config({ path: path.join(process.cwd(), 'environments', `${process.env.MODE}dev.env`)})
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
-//require('dotenv').config({ path: path.join(process.cwd(), 'environments', `${process.env.MODE}.env`)})
-//require('dotenv').config({ path: path.join(process.cwd(), 'environments', `.env`)})
+require('dotenv').config({ path: path.join(process.cwd(), 'environments', `dev.env`)})
 
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRouter');
